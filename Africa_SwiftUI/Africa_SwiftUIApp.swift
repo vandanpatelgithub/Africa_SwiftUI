39741bb00f365ccda0c585fc12d556f3643e33fa
Vandan Patel <vandan.ets@gmail.com>
2021-02-16 18:19:24 -0500

    Initial Commit

---
+++ Africa_SwiftUI/Africa_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_SwiftUIApp.swift
+//  Africa_SwiftUI
+//
+//  Created by Vandan Patel on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Africa_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa_SwiftUI
+//
+//  Created by Vandan Patel on 2/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
